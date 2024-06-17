b569595fb01892e2c3361ce77695bcd97f7a9610
mhfix1337 <matthias.haeberle@infineon.com>
2024-06-17 12:33:20 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function SkillsMember() {
+  return (
+    <div>
+      <h2>Skills</h2>
+      <ul>
+        <li>JavaScript</li>
+        <li>Node.js</li>
+        <li>React</li>
+        <li>Electron</li>
+        <li>Python</li>
+      </ul>
+    </div>
+  );
+}
